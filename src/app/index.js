ceb2a6b01cb392252c8a548a8029c8f4a4e65d1f
YulikK <yulikpoch@gmail.com>
2025-04-08 18:46:37 +0200

    Updates

--- src/app/index.js
+++
@@ -1,8 +0,0 @@
-import '@/app/favicon.png';
-import '@/app/styles.scss';
-import Calculator from '@/widgets/calculator/calculator';
-
-const { body } = document;
-
-const calculator = new Calculator();
-body.append(calculator.getNode());
